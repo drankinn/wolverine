2ba47ef6215cdc385c5ff1f7ccb41950cd0e2f81
Lance Andersen <techlanc@gmail.com>
2015-10-15 21:56:00 -0600

    package install requirements

--- setup.py
+++ setup.py
@@ -9,5 +9,12 @@ setup(
     license='MIT 2.0',
     author='Lance Andersen',
     author_email='techlance@gmail.com',
-    description='micro service framework with python 3.5 and asyncio'
+    description='micro service framework with python 3.5 and asyncio',
+    install_requires=[
+        'aiohttp>=0.17.4',
+        'aiozmq>=0.7.1',
+        'msgpack-python>=0.4.6',
+        'python-consul>=0.4.5',
+        'pyzmq>=14.4.1'
+    ]
 )
