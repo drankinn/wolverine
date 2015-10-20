7cfa9a803d68e99c31adb05a60f9505c9886a468
Lance Andersen <techlanc@gmail.com>
2015-10-20 15:45:19 -0600

    version bump - gateway loop is complete ?

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='wolverine',
-    version='0.1.9',
+    version='0.2.0',
     packages=find_packages(),
     url='http://github.com/drankinn/wolverine',
     license='MIT 2.0',
