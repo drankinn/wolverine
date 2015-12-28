dce035b262a33cede1964ecf0d26f2253750b7cf
Lance Andersen <techlanc@gmail.com>
2015-12-28 09:27:37 -0700

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='wolverine',
-    version='0.3.2',
+    version='0.3.3',
     packages=find_packages(),
     url='http://github.com/drankinn/wolverine',
     license='MIT 2.0',
